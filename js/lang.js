c3e2c9a7b532e8b7aa37dcaaa6309ca933d4e85e
Lucas Lozardo <lucas_losardo@hotmail.com>
2025-08-03 13:40:28 -0300

    Criado os packages com as implementações, js com o arquivo lang.js e o lang com os arquivos en.json e pt.json

---
+++ js/lang.js
@@ -0,0 +1,33 @@
+const userLang = navigator.language || navigator.language;
+if(userLang.startsWith("pt")){
+    loadLanguage("pt");
+} else {
+    loadLanguage("en");
+}
+
+async function loadLanguage(lang) {
+    const response = await fetch(`./lang/${lang}.json`);
+    const data = await response.json();
+
+    document.getElementById("about")?.innerText = data.headerAndFooter.about;
+    document.getElementById("footer")?.innerText = data.headerAndFooter.footer;
+
+    const path = window.location.pathname;
+
+    if(path.includes("index") || path == "/" || path == "/index"){
+        document.getElementById("title")?.innerText = data.index.title;
+        document.getElementById("description")?.innerText = data.index.description;
+        document.getElementById("contact")?.innerText = data.index.contact;
+    }
+
+    if(path.includes("about") || path == "/about"){
+        document.getElementById("titleAbout")?.innerText = data.about.titleAbout;
+        document.getElementById("descriptionP1")?.innerText = data.about.descriptionP1;
+        document.getElementById("descriptionP2")?.innerText = data.about.descriptionP2;
+        document.getElementById("descriptionP3")?.innerText = data.about.descriptionP3;
+    }
+}
+
+function setLanguage(lang){
+    loadLanguage(lang);
+}
